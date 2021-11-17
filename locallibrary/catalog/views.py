fe49894f06bdf41bb2fab1cb7be7ef2fecfd77a2
Andres S <andreysyniy@gmail.com>
2021-11-18 00:17:27 +0200

    Session: Add page visit times.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -11,17 +11,23 @@ def index(request):
     # Генерация "количеств" некоторых главных объектов
     num_books=Book.objects.all().count()
     num_instances=BookInstance.objects.all().count()
+
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
     num_genres = Genre.objects.count()
     num_books_words_in_title = Book.objects.filter(title__iregex=r'\w*').count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres, 'num_books_words_in_title': num_books_words_in_title},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genres':num_genres, 'num_books_words_in_title': num_books_words_in_title, 'num_visits':num_visits},
     )
 
 
